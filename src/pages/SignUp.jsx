eaf374c997f88ea84f26d61e156b473e40d7fed1
kareem ibrahem <kareemibrahem2002@gmail.com>
2024-08-20 02:11:47 +0300

    repair signup (async, handleImaegChange, formData)

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -8,81 +8,90 @@ import { EyeOutlined, EyeInvisibleOutlined, CheckOutlined } from "@ant-design/ic
 const nameRegex = /^[0-9A-Za-z]{6,16}$/;
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 const passRegex =  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/;
-
+const phoneRegex = /^01[0-2,5]{1}[0-9]{8}$/
 
 const SignUp = function() {
     const [inputType, setInputType] = useState(false);
 
     const [nameError, setNameError] = useState();
     const [emailError, setEmailError] = useState();
+    const [phoneError, setPhoneError] = useState();
     const [passError, setPassError] = useState();
-    const [file, setFile] = useState();
     const [success, setSuccess] = useState();
+    const [file, setFile] = useState();
     const [formData, setFormData] = useState({
-        username: "",
+        name: "",
         email: "",
+        phone: "",
         password: "",
         image: null
     });
 
-    const typeHandler = function() {
-        setInputType(!inputType)
-    }
-    const handleValidation = function(e) {
-        const { name, value } = e.target;
-        setFormData((formData) => ({
-            ...formData,
-            [name]: value
-        }))
-        
-        if (name == 'username') {
-            if (nameRegex.test(value) || value == '') {
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+
+        if (e.target.name == 'name') {
+            if (nameRegex.test(e.target.value) || e.target.value == '') {
                 setNameError(false);
-            }else if (!nameRegex.test(value)) {
+            }else if (!nameRegex.test(e.target.value)) {
                 setNameError(true);
             }
         }
         
-        if (name == 'password') {
-            if (passRegex.test(value) || value == '') {
-                setPassError(false);
-            }else if (!passRegex.test(value)) {
-                setPassError(true);
-            }
-        }
-        
-        if (name == 'email') {
-            if (emailRegex.test(value) || value == '') {
+        if (e.target.name == 'email') {
+            if (emailRegex.test(e.target.value) || e.target.value == '') {
                 setEmailError(false)
-            }else if (!emailRegex.test(value)) {
+            }else if (!emailRegex.test(e.target.value)) {
                 setEmailError(true)
             }
         }
         
-        if (name == 'image') {
-            setFile(URL.createObjectURL(e.target.files[0]));
+        if (e.target.name == 'phone') {
+            if (phoneRegex.test(e.target.value) || e.target.value == '') {
+                setPhoneError(false)
+            }else if (!phoneRegex.test(e.target.value)) {
+                setPhoneError(true)
+            }
         }
+
+        if (e.target.name == 'password') {
+            if (passRegex.test(e.target.value) || e.target.value == '') {
+                setPassError(false);
+            }else if (!passRegex.test(e.target.value)) {
+                setPassError(true);
+            }
+        }
+        
+      };
+
+    const typeHandler = function() {
+        setInputType(!inputType)
     }
 
-    const handleSubmit = function(e) {
+    const handleImageChange = (e) => {
+        const file = e.target.files[0]
+        setFormData({ ...formData, image: file });
+        setFile(URL.createObjectURL(file));
+        console.log(file)
+    };
+
+    const handleSubmit = async (e) => {
         e.preventDefault()
-        if (!nameError && !emailError && !passError && file) {
-        fetch('https://jsonplaceholder.typicode.com/users', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json',
-                app_id: '0JyYiOQXQQr5H9OEn21312'
-            },
-            body: JSON.stringify({
-                username: formData.username,
-                email: formData.email,
-                password: formData.password,
-                image: formData.image
-            })
-        })
-        .then(res => res.json())
-        .then(console.log);
-        setSuccess(true);
-    }
+        const data = new FormData();
+        data.append("name", formData.name);
+        data.append("email", formData.email);
+        data.append("phone", formData.phone);
+        data.append("password", formData.password);
+        data.append("image", formData.image);
+    
+            const response = await fetch("https://www.appssquare.sa/api/submit", {
+                method: "POST",
+                body: data,
+            });
+            
+            const result = await response.json();
+            console.log(result);
+            setSuccess(true);
 }
 useEffect(() => {
     if (success) {
@@ -112,19 +121,25 @@ useEffect(() => {
         <div className="flex flex-col space-y-4">
             <div>
             <label htmlFor="name" className="font-semibold text-sm">Name <span className="text-red-600">*</span></label>
-            <input onChange={handleValidation} name="username" type="text" id="name" placeholder="Enter your name" required className="peer p-2 w-full border-2 border-[#E5E7EB] rounded-[5px] block placeholder:text-xs focus:outline-none"/>
+            <input onChange={handleChange} name="name" type="text" id="name" placeholder="Enter your name" required className="peer p-2 w-full border-2 border-[#E5E7EB] rounded-[5px] block placeholder:text-xs focus:outline-none"/>
             {nameError ? <span id="nameAlert" className="text-xs text-red-600">Username should be 6-16 characters and shouldn't include any special characters</span> : null}
             </div>
 
             <div>
             <label htmlFor="email" className="font-semibold text-sm">Email <span className="text-red-600">*</span></label>
-            <input onChange={handleValidation} name="email" type="email" id="email" placeholder="Enter your email" required className="p-2 w-full border-2 border-[#E5E7EB] rounded-[5px] block placeholder:text-xs focus:outline-none"/>
+            <input onChange={handleChange} name="email" type="email" id="email" placeholder="Enter your email" required className="p-2 w-full border-2 border-[#E5E7EB] rounded-[5px] block placeholder:text-xs focus:outline-none"/>
             {emailError ? <span className="text-xs text-red-600">It should be a valid email address</span> : null}
             </div>
 
             <div className="relative">
+            <label htmlFor="phone" className="font-semibold text-sm">Password <span className="text-red-600">*</span></label>
+            <input onChange={handleChange} name="phone" type="tel" id="phone" placeholder="Enter phone number" required className="p-2 w-full border-2 border-[#E5E7EB] rounded-[5px] block placeholder:text-xs focus:outline-none"/>
+            {phoneError ? <span id="nameAlert" className="text-xs text-red-600">Enter a valid phone number</span> : null}
+            </div>
+
+            <div className="relative">
             <label htmlFor="password" className="font-semibold text-sm">Password <span className="text-red-600">*</span></label>
-            <input onChange={handleValidation} name="password" type={inputType ? 'text' : 'password'} id="password" placeholder="Enter password" required className="p-2 w-full border-2 border-[#E5E7EB] rounded-[5px] block placeholder:text-xs focus:outline-none"/>
+            <input onChange={handleChange} name="password" type={inputType ? 'text' : 'password'} id="password" placeholder="Enter password" required className="p-2 w-full border-2 border-[#E5E7EB] rounded-[5px] block placeholder:text-xs focus:outline-none"/>
             {inputType ? <EyeInvisibleOutlined className="absolute top-9 left-[calc(100%_-_32px)] text-xl" onClick={typeHandler}/> : <EyeOutlined className="absolute top-9 left-[calc(100%_-_32px)] text-xl" onClick={typeHandler}/>}
             {passError ? <span className="text-xs text-red-600">Password should be 8-20 characters and include atleast 1 letter, 1 number and 1 special character</span> : null}
             </div>
@@ -134,7 +149,7 @@ useEffect(() => {
         <div className="mt-8 mb-4"> 
         <label htmlFor="image" className="w-32 block bg-[#E5E7EB] p-2 text-center rounded-[5px] cursor-pointer">Select image</label>  
         <div className="flex justify-center items-center mt-2 w-32">
-        <input onInput={handleValidation} name="image" type="file" id="image" accept="image/*" className={`${file ? `block` : 'hidden'} overflow-hidden w-[88px] file:hidden text-xs`}/>
+        <input onChange={handleImageChange} name="image" type="file" id="image" accept="image/*" className={`${file ? `block` : 'hidden'} overflow-hidden w-[88px] file:hidden text-xs`}/>
         {file && <img className=" w-10 h-10" src={file} alt="" />}
         </div> 
         </div>
